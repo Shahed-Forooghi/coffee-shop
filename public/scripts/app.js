caba7354bcf9aae709bb4e34b539b92c92418cce
Shahed <www.shahedforooghi@gmail.com>
2024-01-27 16:40:29 +0330

    Finished template and design for mobile divices and The site menu was made responsive for  , sm:640px , its mean for mobile but still not ready cilcked listener .

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,33 @@
-const darkModeBtn = document.querySelector('#darkmode-btn')
-const darkModeIcon = document.querySelector('#darkmode-icon')
+const darkModeBtns = document.querySelectorAll('.darkmode-btn')
+const darkModeIcons = document.querySelectorAll('.darkmode-icon')
 const htmlELem = document.querySelector('html')
+const submenuOpenBtn = document.querySelector('.submenu-open-btn')
+const submenu = document.querySelector('.submenu')
+const hamburgerMeuBtn = document.querySelector("#hamburger-meu-btn")
+const hamburgerMeu = document.querySelector('#hamburger-meu')
 
-darkModeBtn.addEventListener('click' , event => {
-    event.preventDefault();
-    console.log();
-    console.log(darkModeIcon.attributes.href);
-    if (htmlELem.classList.value === "dark") {
-        htmlELem.classList.remove('dark')
-        darkModeIcon.setAttribute('href' , '#moon')
-    }else{
-        htmlELem.classList.add('dark')
-        darkModeIcon.setAttribute('href' , '#sun')
-    }
+darkModeBtns.forEach(btn => {
+    btn.addEventListener('click' , event => {
+        event.preventDefault();
+        if (htmlELem.classList.value === "dark") {
+            htmlELem.classList.remove('dark')
+            darkModeIcons.forEach(item => {
+                item.setAttribute('href' , '#moon')
+            })
+        }else{
+            htmlELem.classList.add('dark')
+            darkModeIcons.forEach(item => {
+                item.setAttribute('href' , '#sun')
+            })
+        }
+    })
 })
+
+submenuOpenBtn.addEventListener('click' , event => {
+    submenuOpenBtn.classList.toggle('rotate-90')
+    event.currentTarget.parentElement.classList.toggle('text-orange-300')
+    submenu.classList.toggle('submenu--open')
+
+})
+
     
