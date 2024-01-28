efa9dc35bc5632f3b7cc042f06005c065ba8c08b
Shahed <www.shahedforooghi@gmail.com>
2024-01-28 12:15:22 +0330

    finished header template and styles and scripts for all divices .

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,18 @@
 const darkModeBtns = document.querySelectorAll('.darkmode-btn')
 const darkModeIcons = document.querySelectorAll('.darkmode-icon')
+const cartBtnMobile = document.querySelector('#cart-btn')
+const mobileShoppingCart = document.querySelector('#mobile-shopping-cart')
 const htmlELem = document.querySelector('html')
 const submenuOpenBtn = document.querySelector('.submenu-open-btn')
 const submenu = document.querySelector('.submenu')
-const hamburgerMeuBtn = document.querySelector("#hamburger-meu-btn")
-const hamburgerMeu = document.querySelector('#hamburger-meu')
+const hamburgerMeuBtn = document.querySelector("#hamburger-menu-btn")
+const hamburgerMeu = document.querySelector('#hamburger-menu')
+const closeHamburgerMenuBtn = document.querySelector('#closeHamburgerMenuBtn')
+const cartCloseBtn = document.querySelector('#cart-close-btn')
+const overlay = document.querySelector(".overlay")
+
+console.log(cartBtnMobile);
+console.log(mobileShoppingCart);
 
 darkModeBtns.forEach(btn => {
     btn.addEventListener('click' , event => {
@@ -30,4 +38,32 @@ submenuOpenBtn.addEventListener('click' , event => {
 
 })
 
-    
+hamburgerMeuBtn.addEventListener('click' , event => {
+    event.preventDefault()
+    hamburgerMeu.classList.remove('hidden')
+    hamburgerMeu.classList.add('fixed')
+    overlay.classList.remove('hidden')
+})
+
+closeHamburgerMenuBtn.addEventListener('click' , event => {
+    event.preventDefault()
+    hamburgerMeu.classList.remove('fixed')
+    hamburgerMeu.classList.add('hidden')
+    overlay.classList.add('hidden')
+})
+
+cartBtnMobile.addEventListener('click' , event => {
+    event.preventDefault()
+    mobileShoppingCart.classList.remove('hidden')
+    mobileShoppingCart.classList.add('flex')
+    overlay.classList.remove('hidden')
+})
+
+cartCloseBtn.addEventListener('click' , event => {
+    event.preventDefault()
+    mobileShoppingCart.classList.remove('flex')
+    mobileShoppingCart.classList.add('hidden')
+    overlay.classList.add('hidden')
+
+})
+
